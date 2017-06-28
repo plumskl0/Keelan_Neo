47b046498e002818f54fd68cedfe73a1ed117907
freiSMS <Andre.Heraucourt@web.de>
2017-06-28 17:50:20 +0200

    Erster Entwurf des Zielbildes + Triggerebene erstellt

---
+++ Assets/Scripts/zielbereichErreicht.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zielbereichErreicht : MonoBehaviour {
+    public Canvas endbildschirm;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "zielbereich")
+        {
+            Debug.Log("LadeBild");
+            endbildschirm.enabled = true;
+        }
+    }
+}
