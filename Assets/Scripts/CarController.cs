3cf898f6e57cbf8a4b840b877e4d06033b4efab1
freiSMS <Andre.Heraucourt@web.de>
2017-07-01 17:05:38 +0200

    AlternateCarController mit WiimoteSteuerung erweitert

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,17 +7,29 @@ using WiimoteApi;
 
 public class CarController : MonoBehaviour
 {
+    //Variablen aus AlternateCarController
+    public float maxAngle = 30f;
+    public float maxTorque = 300f;
+    public float brakeTorque = 30000f;
 
-    private const int FRONT_RIGHT = 1;
-    private const int FRON_LEFT = 0;
+    public float maxSpeed = 30f;
 
     public Transform[] wheels;
-    public float motorPower = 150.0f;
-    public float maxTurn = 25.0f;
 
-    float torque = 0.0f;
-    float brake = 0.0f;
-    float wheelTurn = 0.0f;
+    private const int FRONT_LEFT = 0;
+    private const int FRONT_RIGHT = 1;
+    private const int REAR_RIGHT = 2;
+    private const int REAR_LEFT = 3;
+
+    private float criticalSpeed = 5f;
+    private int stepsBelow = 5;
+    private int stepsAbove = 1;
+
+    private Rigidbody rb;
+    //****Ende******
+
+
+    
 
     //public int tellerSmoothinFactor = 5;    //glättet Messwerte über die angegebene Anzahl von Frames
     //public int minAenderungswinkel = 5;     //nur wenn sich die Neigung, um mindestens diesen Winkel aendert wird die Tellerneigung veraendert
@@ -34,8 +46,7 @@ public class CarController : MonoBehaviour
         // Gefühl etwas besser wird beim fahren. Einfach in Doku nachlesen für genauere Infos.
         // m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
 
-        myRigidbody = this.gameObject.GetComponent<Rigidbody>();
-        myRigidbody.centerOfMass = new Vector3(0, 0.0f, 0.0f);
+        rb = this.gameObject.GetComponent<Rigidbody>();
         wiiRemote = wiiKalibrierung.wiiRemote;
     }
 
@@ -49,6 +60,9 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
+        float angle;
+        float torque;
+        float handBrake;
         //nutze die Wiimote, falls eine gefunden wurde
         if (wiiRemote != null)
         {
@@ -86,108 +100,119 @@ public class CarController : MonoBehaviour
                 moveVertical = -1;
             }
 
-            torque = moveVertical * motorPower;
-            wheelTurn = moveHorizontal* maxTurn;
-            brake = wiiRemote.Button.a ? myRigidbody.mass * 0.5f : 0.0f;
+            torque = moveVertical * maxTorque;
+            angle = moveHorizontal* maxAngle;
+            handBrake = wiiRemote.Button.a ? brakeTorque : 0;
+            
 
 
-        //    float accel_x;
-        //    float accel_y;
-        //    float accel_z;
+            //    float accel_x;
+            //    float accel_y;
+            //    float accel_z;
 
-        //    float[] accel = wiiRemote.Accel.GetCalibratedAccelData();
-        //    accel_x = accel[0];
-        //    accel_y = -accel[2];
-        //    accel_z = accel[1];
+            //    float[] accel = wiiRemote.Accel.GetCalibratedAccelData();
+            //    accel_x = accel[0];
+            //    accel_y = -accel[2];
+            //    accel_z = accel[1];
 
-        //    Transform cage = transform.Find("Cage");
+            //    Transform cage = transform.Find("Cage");
 
-        //    //Ueberspringe #Tiltaenderungen definiert durch tellerSmoothingFactor
-        //    //Berechne den Mittelwert der uebersprungenen Werte
+            //    //Ueberspringe #Tiltaenderungen definiert durch tellerSmoothingFactor
+            //    //Berechne den Mittelwert der uebersprungenen Werte
 
-        //    frameSummeZ += -accel_z;
-        //    frameSummeX += -accel_x;
-        //    Debug.Log("frameSummeZ: " + frameSummeZ);
-        //    Debug.Log("frameSummeX: " + frameSummeX);
-        //    frameCount++;
+            //    frameSummeZ += -accel_z;
+            //    frameSummeX += -accel_x;
+            //    Debug.Log("frameSummeZ: " + frameSummeZ);
+            //    Debug.Log("frameSummeX: " + frameSummeX);
+            //    frameCount++;
 
 
-        //    //Ansonsten ändere Tilt
-        //    if (!(frameCount < tellerSmoothinFactor))
-        //    {
-        //        //Cage nach links und rechts kippen
-        //        float z = (frameSummeZ/tellerSmoothinFactor) * 90;
-        //        //cage.localRotation = Quaternion.Euler(0f, 0f, z);
+            //    //Ansonsten ändere Tilt
+            //    if (!(frameCount < tellerSmoothinFactor))
+            //    {
+            //        //Cage nach links und rechts kippen
+            //        float z = (frameSummeZ/tellerSmoothinFactor) * 90;
+            //        //cage.localRotation = Quaternion.Euler(0f, 0f, z);
 
-        //        //Cage nach vorne und hinten kippen
-        //        float x = (frameSummeX / tellerSmoothinFactor) * 90;
-        //        Debug.Log("Verändere Tellerwinkel");
-        //        //cage.localRotation = Quaternion.Euler(0f, 0f, z);
+            //        //Cage nach vorne und hinten kippen
+            //        float x = (frameSummeX / tellerSmoothinFactor) * 90;
+            //        Debug.Log("Verändere Tellerwinkel");
+            //        //cage.localRotation = Quaternion.Euler(0f, 0f, z);
 
-        //        //Ignoriere Ausreiser indem nur Neigungen berücksichtigt werden, welche den Winkel um mindestens x Grad veraendert
-        //        float aktuelleNeigungX = cage.eulerAngles.x;
-        //        float aktuelleNeigungZ = cage.eulerAngles.z;
+            //        //Ignoriere Ausreiser indem nur Neigungen berücksichtigt werden, welche den Winkel um mindestens x Grad veraendert
+            //        float aktuelleNeigungX = cage.eulerAngles.x;
+            //        float aktuelleNeigungZ = cage.eulerAngles.z;
 
-        //        if (Math.Abs(aktuelleNeigungX - x) > minAenderungswinkel || Math.Abs(aktuelleNeigungZ - z) > minAenderungswinkel)
-        //        {
-        //            cage.localRotation = Quaternion.Euler(x, 0f, z);
-        //            Debug.Log(this.GetAccelVector());
-        //        }
+            //        if (Math.Abs(aktuelleNeigungX - x) > minAenderungswinkel || Math.Abs(aktuelleNeigungZ - z) > minAenderungswinkel)
+            //        {
+            //            cage.localRotation = Quaternion.Euler(x, 0f, z);
+            //            Debug.Log(this.GetAccelVector());
+            //        }
 
-        //        //Setze Mittelwert und Frame Count auf 0 fuer naechste Runde auf 0
-        //        frameSummeX = 0;
-        //        frameSummeZ = 0;
-        //        frameCount = 0;
-        //    }
- 
+            //        //Setze Mittelwert und Frame Count auf 0 fuer naechste Runde auf 0
+            //        frameSummeX = 0;
+            //        frameSummeZ = 0;
+            //        frameCount = 0;
+            //    }
+
         }
 
         //ansonsten nutzen die Tastatursteuerung
         else
         {
-			torque = Input.GetAxis("Vertical") * motorPower;
-			wheelTurn = Input.GetAxis("Horizontal") * maxTurn;
-			brake = Input.GetKey("space") ? myRigidbody.mass * 0.5f : 0.0f;
+            angle = maxAngle * Input.GetAxis("Horizontal");
+            torque = maxTorque * Input.GetAxis("Vertical");
+            handBrake = Input.GetKey(KeyCode.Space) ? brakeTorque : 0;
         }
-		
-		
-        //turn collider
-        getCollider(0).steerAngle = wheelTurn;
-        getCollider(1).steerAngle = wheelTurn;
-        
-        //turn wheels
-        wheels[0].localEulerAngles = new Vector3(wheels[0].localEulerAngles.x,
-            getCollider(0).steerAngle - wheels[0].localEulerAngles.z + 90,
-            wheels[0].localEulerAngles.z);
-        wheels[1].localEulerAngles = new Vector3(wheels[1].localEulerAngles.x,
-            getCollider(1).steerAngle - wheels[1].localEulerAngles.z + 90,
-            wheels[1].localEulerAngles.z);
 
-        //spin wheels
-        wheels[0].Rotate(0, -getCollider(0).rpm / 60 * 360 * Time.deltaTime, 0);
-        wheels[1].Rotate(0, -getCollider(1).rpm / 60 * 360 * Time.deltaTime, 0);
-        wheels[2].Rotate(0, -getCollider(2).rpm / 60 * 360 * Time.deltaTime, 0);
-        wheels[3].Rotate(0, -getCollider(3).rpm / 60 * 360 * Time.deltaTime, 0);
 
-        //brakes
-        if (brake > 0.0f)
+        getCollider(FRONT_LEFT).ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+        // Höchstgeschwindigkeit des Autos
+        if (rb.velocity.magnitude >= maxSpeed)
         {
-            getCollider(0).brakeTorque = brake;
-            getCollider(1).brakeTorque = brake;
-            getCollider(2).brakeTorque = brake;
-            getCollider(3).brakeTorque = brake;
-            getCollider(2).motorTorque = 0.0f;
-            getCollider(3).motorTorque = 0.0f;
+            rb.velocity = rb.velocity.normalized * maxSpeed;
         }
-        else
-        {
-            getCollider(0).brakeTorque = 0.0f;
-            getCollider(1).brakeTorque = 0.0f;
-            getCollider(2).brakeTorque = 0.0f;
-            getCollider(3).brakeTorque = 0.0f;
-            getCollider(2).motorTorque = torque;
-            getCollider(3).motorTorque = torque;
-        }
+
+        // Vordere Reifen lenken
+        getCollider(FRONT_LEFT).steerAngle = angle;
+        getCollider(FRONT_RIGHT).steerAngle = angle;
+
+        // Bremsen mit allen Rädern
+        fullBrake(handBrake);
+
+        // Antrieb auf alle Räder?
+        getCollider(FRONT_LEFT).motorTorque = torque;
+        getCollider(FRONT_RIGHT).motorTorque = torque;
+        getCollider(REAR_RIGHT).motorTorque = torque;
+        getCollider(REAR_LEFT).motorTorque = torque;
+
+        // Räder bewegen
+        moveWheels(getCollider(FRONT_LEFT));
+        moveWheels(getCollider(FRONT_RIGHT));
+        moveWheels(getCollider(REAR_RIGHT));
+        moveWheels(getCollider(REAR_LEFT));
+
+    }
+
+    public void fullBrake(float handBrake)
+    {
+        getCollider(FRONT_LEFT).brakeTorque = handBrake;
+        getCollider(FRONT_RIGHT).brakeTorque = handBrake;
+        getCollider(REAR_RIGHT).brakeTorque = handBrake;
+        getCollider(REAR_LEFT).brakeTorque = handBrake;
+    }
+
+    private void moveWheels(WheelCollider wheel)
+    {
+        Quaternion q;
+        Vector3 p;
+        wheel.GetWorldPose(out p, out q);
+
+        // Assume that the only child of the wheelcollider is the wheel shape.
+        Transform shapeTransform = wheel.transform.GetChild(0);
+        shapeTransform.position = p;
+        shapeTransform.rotation = q;
     }
 
     WheelCollider getCollider(int n)
@@ -205,26 +230,26 @@ public class CarController : MonoBehaviour
     }
 
         //wiiMoteHilfsmethoden
-        void InitWiimotes()
-    {
-        WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+    //    void InitWiimotes()
+    //{
+    //    WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
 
-        foreach (Wiimote remote in WiimoteManager.Wiimotes)
-        {
-            // Do stuff.
-            //Console.WriteLine("Gefunden");
+    //    foreach (Wiimote remote in WiimoteManager.Wiimotes)
+    //    {
+    //        // Do stuff.
+    //        //Console.WriteLine("Gefunden");
 
-            //LEDs markieren, dass WiiRemote verbunden ist
-            wiiRemote = remote;
-            //remote.RumbleOn = true;
-            remote.SendStatusInfoRequest();
-            wiiRemote.SendPlayerLED(true, false, false, false);
-            //Thread.Sleep(1000);
-            //remote.RumbleOn = false;
-            remote.SendStatusInfoRequest();
-            remote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
-        }
-    }
+    //        //LEDs markieren, dass WiiRemote verbunden ist
+    //        wiiRemote = remote;
+    //        //remote.RumbleOn = true;
+    //        remote.SendStatusInfoRequest();
+    //        wiiRemote.SendPlayerLED(true, false, false, false);
+    //        //Thread.Sleep(1000);
+    //        //remote.RumbleOn = false;
+    //        remote.SendStatusInfoRequest();
+    //        remote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
+    //    }
+    //}
 
     private Vector3 GetAccelVector()
     {
