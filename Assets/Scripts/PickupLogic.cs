42325ec3507fdf0a4a969b35d9652a935e37ad16
plumskl0 <plumskl0@users.noreply.github.com>
2017-07-02 17:06:35 +0200

    PickupLogic angepasst und GUI vorbereitet

--- Assets/Scripts/PickupLogic.cs
+++ Assets/Scripts/PickupLogic.cs
@@ -9,6 +9,27 @@ public class PickupLogic : MonoBehaviour {
 
     public Text CoinCountText;
 
+    public Text TimerText;
+
+    private float time;
+
+    private void Start()
+    {
+        CoinCountText.text = " ";
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        var minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+        var seconds = time % 60;//Use the euclidean division for the seconds.
+        var fraction = (time * 100) % 100;
+
+        //update the label value
+        TimerText.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+    }
+
     private void OnTriggerEnter(Collider other)
     {        
         if (other.CompareTag("Coin"))
@@ -19,9 +40,8 @@ public class PickupLogic : MonoBehaviour {
         }
     }
 
-
     private void setText()
     {
-        CoinCountText.text = "Aufgesammelte Coins" + CoinCount;
+        CoinCountText.text = "Münzen " + CoinCount;
     }
 }
