34571e21774bb6f1d269efa0facc21ffa7c18b7c
freiSMS <Andre.Heraucourt@web.de>
2017-07-02 17:12:00 +0200

    Entwurf des Zielbildschirm erstellt
    Die Variablen des Scriptes von Mareks Script werden jetzt beim Spielende angezeigt

--- Assets/Scripts/zielbereichErreicht.cs
+++ Assets/Scripts/zielbereichErreicht.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class zielbereichErreicht : MonoBehaviour {
     public Canvas endbildschirm;
+    public int coinCount;
+    private int timeNeeded;
+
+    public Text coinCountText;
+    public Text timeNeededText;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("zielbereich"))
         {
             Debug.Log("LadeBild");
+            coinCount = this.GetComponent<PickupLogic>().CoinCount;
+            timeNeeded = this.GetComponent<PickupLogic>().CoinCount;
+
+            coinCountText.text = coinCount.ToString();
+            timeNeededText.text = timeNeeded.ToString();
             endbildschirm.enabled = true;
         }
     }
