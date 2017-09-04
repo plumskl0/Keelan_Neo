c46e10907aaac1187bb074634e02a29826f220aa
freiSMS <Andre.Heraucourt@web.de>
2017-09-04 14:36:41 +0200

    Animation in Start und Forfuehrende aufgeteilt, Positionsverschiebung gefixed

---
+++ Assets/Scripts/rotateTurbine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateTurbine : MonoBehaviour {
+    Animation animation;
+	// Use this for initialization
+	void Start () {
+        //animation.Play
+
+        animation = GetComponent<Animation>();
+        //  .animation.Play()
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            animation.Play("Fluegel|startRotation");
+            Debug.Log("weiter");
+            /*while (animation.IsPlaying("Fluegel|startRotation"))
+            {
+                Debug.Log("starteFluegel");
+            }*/
+            animation.Play("Fluegel|continuesRotation");
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("starte Rotation");
+            animation.Play("Fluegel|startRotation");
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("führe Rotation fort");
+            animation.Play("Fluegel|continuesRotation");
+        }
+		
+	}
+}
