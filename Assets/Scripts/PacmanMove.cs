a38cb4ae416df1e9c0c56fa9b72e444405e03482
AlMahamid <muhammadalmahamid@gmail.com>
2017-09-14 17:25:55 +0200

    testeHinzufuegenFehlenderTeile

---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour {
+
+	private Rigidbody regPacman;
+	public float PacmanSpeed = 10f;
+	void Start () {
+		regPacman = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		regPacman.velocity = new Vector2 (transform.localScale.x, 0) * PacmanSpeed;
+	}
+}
---
+++ Assets/Scripts/PacmanRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanRotate : MonoBehaviour {
+
+	public Transform PacmanTransform, SensorTransform;
+	private bool Collisons = false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Collisons = Physics.Linecast (PacmanTransform.position, SensorTransform.position, 1 << LayerMask.NameToLayer ("solid"));
+
+		if (Collisons)
+			transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+	}
+}
